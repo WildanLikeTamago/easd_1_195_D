42c98f43a9b61dbedc5c7de0b86f971d73b52b7d
WildanLikeTamago <utoyowildan13039@gmail.com>
2023-04-07 10:10:16 +0700

    Jawaban no.4

--- easd_1_195_D/easd_1_195_D.cpp
+++ easd_1_195_D/easd_1_195_D.cpp
@@ -4,4 +4,4 @@
 
 //3. Kecepatan mesin, sistem operasi, compiler, bahasa pemrograman, ukuran data yang diinput.
 
-//4. Structure, karena lebih mudah untuk saya pahami, dan data yang diinput lebih sederhana dibanding yang materi yang lain yang pernah dijelaskan
+//4. Quick sort, karena pengerjaan lebih sederhana, efisien sehingga tidak membutuhkan waktu banyak, dan data yang diinput ukurannya kecil
