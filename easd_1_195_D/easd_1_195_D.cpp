e582d969278c30ac52abbfa55a4133127431f32d
WildanLikeTamago <utoyowildan13039@gmail.com>
2023-04-07 09:58:35 +0700

    Jawaban no.3

--- easd_1_195_D/easd_1_195_D.cpp
+++ easd_1_195_D/easd_1_195_D.cpp
@@ -1,3 +1,5 @@
 //1. Karena Algoritma sangat penting dalam pemecahan suatu masalah dengan efisien, sistematis, terstruktur.
 
 //2. Statis dan dinamis
+
+//3. Kecepatan mesin, sistem operasi, compiler, bahasa pemrograman, ukuran data yang diinput
